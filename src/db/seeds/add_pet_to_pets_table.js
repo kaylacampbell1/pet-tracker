bd957d76b81758bf3c48c312a919428a4b7078bd
kaylacampbell1 <kaylacampbell3003@gmail.com>
2023-12-11 12:27:15 -0500

    updated images in seed file

--- src/db/seeds/add_pet_to_pets_table.js
+++ src/db/seeds/add_pet_to_pets_table.js
@@ -5,8 +5,20 @@
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('pets').del();
-  await knex('pets').insert([
-    {name: 'Fluffy', image: 'https://i.imgur.com/ny5oJfR.jpg', species: 'cat', friendly: true},
-    {name: 'Lucky', image: 'https://i.imgur.com/ny5oJfR.jpg', species: 'dog', friendly: false},
+  await knex("pets").insert([
+    {
+      name: "Fluffy",
+      image:
+        "https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg",
+      species: "cat",
+      friendly: true,
+    },
+    {
+      name: "Lucky",
+      image:
+        "https://www.hartz.com/wp-content/uploads/2022/04/small-dog-owners-1.jpg",
+      species: "dog",
+      friendly: false,
+    },
   ]);
 };
